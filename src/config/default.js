b52f50eb6e7a8c36ab29deddf54c91f764a99e90
haraslub <lubos.harasta@gmail.com>
2022-04-08 13:41:10 +0200

    fix bug in test

--- src/config/default.js
+++ src/config/default.js
@@ -53,6 +53,7 @@ module.exports = env => ({
     // OR:
     connection: {
       connectionString: process.env.DATABASE_URL || 'postgres://postgres:password1234!@localhost:5432/nodejs-nights-local',
+      ssl: process.env.DATABASE_URL ? true : false,
     },
     pool: {
       min: process.env.DATABASE_POOL_MIN || 0,
